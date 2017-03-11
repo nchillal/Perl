bdc4a32e3053030f253af280dbd0f35b5d7d6981
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-03-11 09:21:42 -0800

    Update subnet_mask.pl

--- subnet_mask.pl
+++ subnet_mask.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl 
 # Purpose: Get subnet mask information from IP address and netmask
+# Usage: subnet.pl <IP_ADDRESS> <NETMASK>
 
 use Socket; 
 print inet_ntoa( inet_aton($ARGV[0]) & inet_aton($ARGV[1]));
